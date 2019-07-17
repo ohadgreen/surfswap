7775f6652251c76a43531995756adc92cd3c0e0a
ohadgreen <ohad.green@gmail.com>
2019-07-17 13:22:52 +0300

    minus surfboard find by owner

--- src/main/java/com/acme/surfswap/repositories/SurfboardRepository.java
+++ src/main/java/com/acme/surfswap/repositories/SurfboardRepository.java
@@ -1,9 +1,7 @@
 package com.acme.surfswap.repositories;
 
-import com.acme.surfswap.model.Owner;
 import com.acme.surfswap.model.Surfboard;
 import org.springframework.data.repository.CrudRepository;
 
 public interface SurfboardRepository extends CrudRepository<Surfboard, Long> {
-    Owner findByLastName(String lastName);
 }
