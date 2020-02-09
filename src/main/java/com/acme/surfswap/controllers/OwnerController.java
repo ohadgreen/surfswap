c5a0e929fb511a5080bff75cd58e666c2b9c3223
Ohad Green <ohad.green@odysii.com>
2020-02-09 13:38:21 +0200

    dev mysql docker setup

--- src/main/java/com/acme/surfswap/controllers/OwnerController.java
+++ src/main/java/com/acme/surfswap/controllers/OwnerController.java
@@ -2,19 +2,20 @@ package com.acme.surfswap.controllers;
 
 
 import com.acme.surfswap.model.Owner;
+import com.acme.surfswap.model.Surfboard;
 import com.acme.surfswap.services.OwnerServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import javax.xml.ws.Response;
 import java.util.Set;
 
 @Slf4j
 @RestController
+@RequestMapping("/api/owners")
 public class OwnerController {
     private final OwnerServiceImpl ownerService;
 
@@ -22,7 +23,7 @@ public class OwnerController {
         this.ownerService = ownerService;
     }
 
-    @GetMapping("api/owners/all")
+    @GetMapping("/all")
     public ResponseEntity<Set<Owner>> allOwners() {
         Set<Owner> allOwners = ownerService.findAll();
         if (allOwners.isEmpty()) {
@@ -31,7 +32,7 @@ public class OwnerController {
         return new ResponseEntity<>(allOwners, HttpStatus.OK);
     }
 
-    @GetMapping("api/owners/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<Owner> ownerById(@PathVariable Long id) {
         log.debug("owner id: " + id);
         Owner owner = ownerService.findById(id);
@@ -39,5 +40,15 @@ public class OwnerController {
             return new ResponseEntity<>(owner, HttpStatus.OK);
         }
         return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
+    }
+
+    @PostMapping("/new")
+    public ResponseEntity<Long> saveNewOwner(@RequestBody Owner owner) {
+        log.debug("new owner: " + owner.toString());
+        Owner savedOwner = ownerService.save(owner);
+        if (savedOwner != null) {
+            return new ResponseEntity<>(savedOwner.getId(), HttpStatus.CREATED);
+        }
+        return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
     }
 }
